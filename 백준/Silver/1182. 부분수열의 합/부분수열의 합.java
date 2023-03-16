432eed81a0cb49d9b6abc90ab78a2b9178cc44bb
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2023-03-16 20:50:45 +0900

    [Silver II] Title: 부분수열의 합, Time: 176 ms, Memory: 12248 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N;
+	static int[] seq;
+	static int S;
+	static int result = 0;
+	static boolean[] flag;
+	static int[] subseq;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		//수열 길이 및 목표 합 입력받기
+		N = Integer.parseInt(st.nextToken());
+		S = Integer.parseInt(st.nextToken());
+		seq = new int[N+1];
+		flag = new boolean[N+1];
+		st = new StringTokenizer(br.readLine());
+		//수열 입력 받기
+		for(int i=1; i<=N; i++) {
+			seq[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for(int i=1; i<=N; i++) {
+			subseq = new int[i+1];
+			findSubseq(1, i, 1);
+		}
+		
+		System.out.println(result);
+
+	}
+	
+	public static void findSubseq(int cnt, int len, int idx) {
+
+		if(cnt == len+1) {
+			int sum =0;
+			idx = 1;
+			for(int j=1; j<=len; j++) {
+				sum +=subseq[j];
+			}
+			if(sum==S) {
+				result++;
+			
+			}
+			return;
+		}
+	
+		for(int j=idx; j<=N; j++) {
+			if(!flag[j] ) {
+				subseq[cnt] = seq[j];
+				flag[j]=true;
+				idx = j;
+				findSubseq(cnt+1, len, idx+1);				
+				flag[j] = false;
+			}else return;
+		}
+	}
+
+}
