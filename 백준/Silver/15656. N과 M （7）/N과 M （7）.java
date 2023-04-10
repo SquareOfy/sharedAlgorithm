4be56b39f16630d2663d176916ae58acc1e6eae5
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2023-04-10 11:56:48 +0900

    [Silver III] Title: N과 M (7), Time: 624 ms, Memory: 220080 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,58 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N;
+	static int M;
+	static int[] numbers;
+	static int[] result;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		
+		st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		numbers = new int[N];
+		result = new int[M];
+	
+		st = new StringTokenizer(br.readLine());
+ 		for(int n=0; n<N; n++) {
+			numbers[n] = Integer.parseInt(st.nextToken());
+		}
+ 		
+ 		Arrays.sort(numbers);
+ 		
+ 		permutation(0);
+ 		
+ 		System.out.println(sb.toString());
+
+	
+ 		
+	}
+	
+	static void permutation(int cnt) {
+		if(cnt ==M) {
+			for(int i=0; i<M; i++) {
+				sb.append(result[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i=0; i<N; i++) {
+			result[cnt] = numbers[i];
+			permutation(cnt+1);
+
+		}
+	}
+	
+
+}
