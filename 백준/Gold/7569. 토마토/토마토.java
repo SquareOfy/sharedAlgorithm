640dbcb7b8afacf6b362b279e25f9c6d3f9fcecd
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2023-04-13 21:03:48 +0900

    [Gold V] Title: 토마토, Time: 624 ms, Memory: 131564 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -1,114 +1,101 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
 public class Main {
-	static class Tomato {
-		int row;
-		int column;
-		int rank;
-		int height;
+static int[] dr = {0,0,-1,1,0,0};
+static int[] dc = {0,0,0,0,-1,1};
+static int[] dh = {-1,1,0,0,0,0};
 
-		Tomato(int h, int r, int c, int rank) {
-			this.height = h;
-			this.row = r;
-			this.column = c;
-			this.rank = rank;
-		}
+static int[][][] map;
+static boolean[][][] visited;
+
+static int H,N,M;
+
+static Queue<Node> queue = new LinkedList<>();
+
+static class Node{
+	int r;
+	int c;
+	int h;
+	int rank;
+	
+	Node(int h, int r, int c, int rank){
+		this.h = h;
+		this.r = r;
+		this.c = c;
+		this.rank = rank;
 	}
-
-	static int M;
-	static int N;
-	static int H;
-	static int[][][] map;
-	static boolean[][][] visited;
-	static int[] dr = { -1, 1, 0, 0, 0, 0 };
-	static int[] dc = { 0, 0, -1, 1, 0, 0 };
-	static int[] dh = { 0, 0, 0, 0, -1, 1 };
-	static Queue<Tomato> queue = new LinkedList<>();
-
+}
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 		StringTokenizer st;
-
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
+		
+		st= new StringTokenizer(br.readLine());
 		M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
 		H = Integer.parseInt(st.nextToken());
-
-		map = new int[H][M][N];
-		visited = new boolean[H][M][N];
-		boolean flag = false;
-		int max = -1;
-		for (int h = 0; h < H; h++) {
-			for (int m = 0; m < M; m++) {
+		
+		map = new int[H][N][M];
+		visited = new boolean[H][N][M];
+		
+		for(int h=0; h<H; h++) {
+			for(int n=0; n<N; n++) {
 				st = new StringTokenizer(br.readLine());
-				for (int n = 0; n < N; n++) {
-					map[h][m][n] = Integer.parseInt(st.nextToken());
-				}
-			}
-		}
-		if (check())
-			flag = true;
-
-		for (int h = 0; h < H; h++) {
-			for (int m = 0; m < M; m++) {
-				for (int n = 0; n < N; n++) {
-					if (map[h][m][n] == 1) {
-						queue.add(new Tomato(h, m, n, 0));
-						visited[h][m][n] = true;
+				for(int m=0; m<M; m++) {
+					map[h][n][m] = Integer.parseInt(st.nextToken());
+					if(map[h][n][m]==1) {
+						queue.add(new Node(h,n,m,0));
+						visited[h][n][m] = true;
 					}
 				}
 			}
 		}
+		
+		System.out.println(BFS());
 
-		max = BFS();
-
-		if (flag)
-			System.out.println(0);
-		else if (check())
-			System.out.println(max);
-		else
-			System.out.println(-1);
 
 	}
 
-	static boolean check() {
-		for (int h = 0; h < H; h++) {
-			for (int m = 0; m < M; m++) {
-				for (int n = 0; n < N; n++) {
-					if (map[h][m][n] == 0 && !visited[h][m][n])
-						return false;
+	static int BFS() {
+		int result =0;
+		while(!queue.isEmpty()) {
+			Node now = queue.poll();
+			result = now.rank;
+			for(int i=0; i<6; i++) {
+				int dk = now.h + dh[i];
+				int du = now.r + dr[i];
+				int dv = now.c + dc[i];
+				
+				if(dk<0 || dk>=H || du <0 || du>=N || dv <0 || dv>=M ) continue;
+				
+				if(!visited[dk][du][dv]&& map[dk][du][dv]==0) {
+					map[dk][du][dv]=1;
+					visited[dk][du][dv] = true;
+					queue.add(new Node(dk, du, dv, now.rank+1));
+				}
+			}
+		}
+		if(checkTomato()) return result;
+		else return -1;
+		
+		
+	}
+	static boolean checkTomato() {
+		for(int h=0; h<H; h++) {
+			for(int r=0; r<N; r++) {
+				for(int c=0; c<M; c++) {
+					if(map[h][r][c] == 0) return false;
 				}
 			}
 		}
 		return true;
 	}
-
-	static int BFS() {
-		int result = 0;
-
-		while (!queue.isEmpty()) {
-			Tomato curr = queue.poll();
-			for (int i = 0; i < 6; i++) {
-				int du = curr.row + dr[i];
-				int dv = curr.column + dc[i];
-				int h = curr.height + dh[i];
-				if (du >= 0 && du < M && dv >= 0 && dv < N && h >= 0 && h < H) {
-					if (!visited[h][du][dv] && map[h][du][dv] == 0) {
-						int rank = curr.rank;
-						queue.add(new Tomato(h, du, dv, rank + 1));
-						visited[h][du][dv] = true;
-						result = Math.max(result, rank + 1);
-					}
-				}
-			}
-		}
-
-		return result;
-	}
-
 }
