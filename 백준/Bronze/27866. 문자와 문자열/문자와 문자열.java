d068b38c5483c2403ea3e38a0f6874e9928c40ef
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2023-04-18 05:45:50 +0900

    [Bronze V] Title: 문자와 문자열, Time: 116 ms, Memory: 12856 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.next();
+		
+		int n = sc.nextInt();
+		
+		System.out.println(str.charAt(n-1));
+
+	}
+
+}
