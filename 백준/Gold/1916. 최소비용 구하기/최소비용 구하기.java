60f2d023b697e3c60e17706057cd1d7bf4bab8cc
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2023-07-03 02:19:32 +0900

    [Gold V] Title: 최소비용 구하기, Time: 332 ms, Memory: 46156 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,44 +1,31 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static class Node {
-		int ed;
-		int num;
-		Node(int ed, int num){
-			this.ed = ed;
-			this.num = num;
-		}
-	}
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
 		int N = Integer.parseInt(br.readLine()); //도시 수(노드 수) 
 		int M = Integer.parseInt(br.readLine()); //버스 수(간선 수) 
-//		int[][] adj = new int[N+1][N+1];
-	
-		
-		List<Node>[] adj = new ArrayList[N+1];
-		for(int i=1; i<=N; i++) {
-			adj[i] = new ArrayList<>();
+		int[][] adj = new int[N+1][N+1];
+		for(int i=1; i<N+1; i++) {
+			
+			Arrays.fill(adj[i], -1);
 		}
-		boolean flag = false;
-		
 		for(int m=0; m<M; m++) {
 			st=new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
-			adj[a].add(new Node(b, c));
+		
+			if(adj[a][b]!= -1 && adj[a][b]<c) continue;
+			adj[a][b]=c;
+		
 		}
 		
 		final int INF = Integer.MAX_VALUE;
@@ -54,30 +41,33 @@ public class Main {
 		Arrays.fill(dist, INF);
 		dist[start] = 0;
 		
-		PriorityQueue<Node> pq = new PriorityQueue<>((o1,o2)-> {
-			return o1.num - o2.num;
-		});
 		
-		
-		pq.add(new Node(start, 0));
-		dist[start] = 0;
-		
-		while(!pq.isEmpty()) {
-			Node tmp = pq.poll();
-			int now = tmp.ed;
-			int nowCost = tmp.num;
-			if(nowCost > dist[now]) continue;
+		for(int n=1; n<=N; n++) {
+			int current = -1;
+			int min = INF;
 			
-			for(Node node : adj[now]) {
-				if(dist[node.ed] > nowCost+node.num) {
-					dist[node.ed]= nowCost+node.num;
-					pq.offer(new Node(node.ed, dist[node.ed]));
+			for(int i=1; i<=N; i++) {
+				if(!visited[i] && min>dist[i]) {
+					min = dist[i];
+					current = i;
 				}
 			}
+			
+			if(current == -1) {
+	
+				break;
+			}
+			visited[current] = true;
+			
+			for(int i=1; i<=N; i++) {
+				if(!visited[i] && adj[current][i] != -1 && dist[i] > min+adj[current][i]) {
+					dist[i] = min + adj[current][i];
+					}
+			}
+			
 		}
+	
 		
-
-
 		System.out.println(dist[end]);
 	}
 
