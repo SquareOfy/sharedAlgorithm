0afbcda6ccf11d4bc8c41052df4d08d8e3b49b8f
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2023-12-09 20:26:51 +0900

    [D3] Title: 평행사변형, Time: 148 ms, Memory: 21,468 KB -BaekjoonHub

---
+++ SWEA/D3/15941. 평행사변형/평행사변형.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        for(int t=1; t<=T; t++){
+            int N = sc.nextInt();
+            System.out.printf("#%d %d\n", t, N*N);
+
+        }
+    }
+}
