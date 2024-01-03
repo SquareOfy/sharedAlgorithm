30e40bb797fdcbc51d4e74b57e876915e7838629
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-01-03 18:34:54 +0900

    [Gold IV] Title: 수 묶기, Time: 76 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Gold/1744. 수 묶기/수 묶기.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] seq = new int[N];
+        boolean[] visited = new boolean[N];
+        int negative = 0;
+        int positive = 0;
+        int sum = 0;
+        for(int n=0; n<N; n++){
+            seq[n] = Integer.parseInt(br.readLine());
+            sum += seq[n];
+            if(seq[n]>0) positive++;
+            else if(seq[n]<=0) negative++;
+        }
+
+        Arrays.sort(seq);
+
+        int tieSum = 0;
+        if(N>1){
+            if(negative%2==0){
+                for(int i=0; i<negative; i+=2){
+                    tieSum += (seq[i]*seq[i+1]);
+                }
+            }else{
+                for(int i=0; i<negative-1; i+=2){
+                    tieSum += (seq[i] * seq[i+1]);
+                }
+                tieSum+= seq[negative-1];
+            }
+
+            if(positive%2==0){
+                for(int i=N-1; i>negative; i-=2){
+                    if(seq[i]==1 || seq[i-1]==1){
+                        tieSum +=(seq[i]+seq[i-1]);
+
+                    }else{
+                        tieSum+= (seq[i]*seq[i-1]);
+                    }
+                }
+            }else{
+                for(int i=N-1; i>negative+1; i-=2){
+                    if(seq[i]==1 || seq[i-1]==1){
+                        tieSum +=(seq[i]+seq[i-1]);
+
+                    }else{
+                        tieSum+= (seq[i]*seq[i-1]);
+                    }
+                }
+                tieSum+=seq[negative];
+            }
+
+            System.out.println(Math.max(tieSum, sum));
+        }else{
+            System.out.println(sum);
+        }
+
+
+
+
+
+    }
+}
