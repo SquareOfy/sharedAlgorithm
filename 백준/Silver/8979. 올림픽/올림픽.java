0f9d6bd8f30d05a7d7d84fd62ad0a2bb82739860
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-05-20 22:04:40 +0900

    [Silver V] Title: 올림픽, Time: 80 ms, Memory: 11520 KB, Score: 20 point -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N,K;
+    static int[][] medals;
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+
+    public static void main(String[] args) throws IOException {
+
+        input();
+
+
+        Arrays.sort(medals, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[0]==o2[0]){
+                    if(o1[1]==o2[1]){
+                        return o2[2]-o1[2];
+                    }
+                    return o2[1]-o1[1];
+                }
+                return o2[0]-o1[0];
+            }
+        });
+
+        medals[0][4]=1;
+
+        outer: for(int i=1; i<N; i++){
+            for(int j=0; j<3; j++){
+                if(medals[i-1][j]>medals[i][j]){
+                    medals[i][4] = medals[i-1][4]+1;
+                    continue outer;
+                }
+            }
+            medals[i][4] = medals[i-1][4];
+        }
+
+        for(int i=0; i<N; i++){
+            if(medals[i][3]==K){
+                sb.append(medals[i][4]);
+                break;
+            }
+        }
+        System.out.println(sb.toString());
+    }
+
+    public static void input() throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+
+        K = Integer.parseInt(st.nextToken());
+        medals = new int[N+1][5];
+        for(int n=0; n<N; n++){
+            st = new StringTokenizer(br.readLine());
+            int idx = Integer.parseInt(st.nextToken());
+            medals[n][3] = idx;
+            for(int i=0; i<3; i++){
+                medals[n][i] = Integer.parseInt(st.nextToken());
+            }
+
+        }
+    }
+}
