5d3a7f65245dd0ceaa3fa6d936b5f499d6631bef
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-07-18 22:35:41 +0900

    [Silver V] Title: 셀프 넘버, Time: 124 ms, Memory: 110408 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -1,13 +1,19 @@
-arr = list([0]*10001)
+self_numbers = [0] * 10001
 
-for i in range(1, 10001):
+for i in range(1,10001):
+    str_num = str(i)
     num = i
-    for s in str(i):
+    flag = True
+    for s in str_num:
         num += int(s)
-    if(num<=10000):
-        arr[num] = 1
+        if(num > 10000):
+            flag = False
+            break
+
+    if(flag and num != i):
+        self_numbers[num] = 1
 
 
 for i in range(1, 10001):
-    if(arr[i]==0):
+    if(self_numbers[i]!=1):
         print(i)
