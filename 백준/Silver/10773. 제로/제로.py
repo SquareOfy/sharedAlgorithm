751a402c713e959d8e7e35c96f2975ea1c992300
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-07-21 20:33:55 +0900

    [Silver IV] Title: 제로, Time: 132 ms, Memory: 114236 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -4,14 +4,12 @@ input = sys.stdin.readline
 
 k = int(input())
 numbers = []
-for _ in range(k):
+
+for i in range(k):
     num = int(input())
-    if(num==0):
+    if num == 0:
         numbers.pop()
     else:
         numbers.append(num)
-result =0
-for i in numbers:
-    result+= i
 
-print(result)
+print(sum(numbers))
