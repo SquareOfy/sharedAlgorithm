3d63784beaadd3543adf2230b2c9ad86177f0796
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-07-22 16:37:13 +0900

    [Silver IV] Title: 연속부분최대곱, Time: 140 ms, Memory: 110880 KB -BaekjoonHub

--- 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.py
+++ 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.py
@@ -1,11 +1,18 @@
 n = int(input())
 
 lst = [float(input()) for _ in range(n)]
-answer = 0
-for i in range(n):
-    multiply = 1.0
-    for j in range(i, n):
-        multiply = multiply * lst[j]
-        answer = max(multiply, answer)
 
+answer = 0
+number = 1.0
+
+for i in lst:
+    number *= i
+    if answer < number:
+        answer = number
+    if number < 1:
+        number = 1
 print(f'{answer:.3f}')
+
+
+
+
