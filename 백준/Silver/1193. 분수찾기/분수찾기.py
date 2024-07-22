36feb88d4f65384653e6ab17084c5144df0166d0
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-07-22 17:10:26 +0900

    [Silver V] Title: 분수찾기, Time: 112 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.py
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -1,30 +1,14 @@
-import sys
-
-input = sys.stdin.readline
-
 n = int(input())
 
-k = 1
-sum = 1
-while(sum<n):
-    k+=1
-    sum = int((k*(k+1))/2)
-
-start = int((k*(k-1))/2) +1
-if(k%2==0):
-    a = 1
-    b = k
-    
-    while(start != n):
-        a += 1
-        b -= 1
-        start += 1
-    print(f'{a}/{b}')
+i = 1
+while 1:
+    if i*(i-1)//2 < n <= i*(i+1)//2:
+        break
+    i+= 1
+start = i*(i-1)//2
+num = n-start
+if i%2 == 0:
+    print(f"{num}/{i+1-num}")
 else:
-    a = k
-    b = 1
-    while(start != n):
-        a -= 1
-        b += 1
-        start+=1
-    print(f'{a}/{b}')
+    print(f"{i+1-num}/{num}")
+
