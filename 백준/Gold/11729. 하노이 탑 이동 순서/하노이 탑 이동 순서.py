38b744375605e7c7f305bda800db6df13fc6aee0
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-07-23 15:47:21 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 804 ms, Memory: 247268 KB -BaekjoonHub

---
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
@@ -0,0 +1,38 @@
+k = int(input())
+
+
+def get_stick(i, j):
+    if 1 not in (i, j):
+        return 1
+    if 2 not in (i, j):
+        return 2
+    if 3 not in (i, j):
+        return 3
+
+
+def hanoi(i, start, end):
+
+    if i == 0:
+        return
+    if i==1:
+        result.append([start, end])
+        return
+    hanoi(i-1, start, get_stick(start,end))
+    result.append([start, end])
+    hanoi(i-1, get_stick(start,end), end)
+
+
+
+result = []
+hanoi(k, 1, 3)
+print(len(result))
+for i in result:
+    print(*i)
+
+
+
+
+
+
+
+
