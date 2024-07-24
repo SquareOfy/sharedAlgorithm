25fa336ee6cf2428b0256f75663158af77cf9669
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-07-24 19:14:41 +0900

    [Silver V] Title: 색종이, Time: 116 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -2,15 +2,13 @@ arr = [[0]*101 for _ in range(100)]
 
 n = int(input())
 papers = [list(map(int, input().split())) for _ in range(n)]
-
+answer =0
 for a in papers:
     for i in range(a[0], a[0]+10):
         for j in range(a[1], a[1]+10):
-            arr[i][j] = 1
-answer =0
-for lst in arr:
-    for i in lst:
-        if i==1:
-            answer+=1
+            if arr[i][j] ==0:
+                answer+= 1
+                arr[i][j] = 1
+
 
 print(answer)
