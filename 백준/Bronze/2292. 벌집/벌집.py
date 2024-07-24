dfa0d82471b0158f527e727d2407f47984372d41
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-07-24 17:23:18 +0900

    [Bronze II] Title: 벌집, Time: 116 ms, Memory: 109540 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.py
+++ 백준/Bronze/2292. 벌집/벌집.py
@@ -1,8 +1,9 @@
-n= int(input ())
-room = 1
-i = 1
-
-while n > room:
-    room += 6 * i
-    i += 1
-print(i)
+n = int(input())
+k = 1
+lst = [1]
+while 1:
+    if lst[-1] >= n:
+        break
+    lst.append(lst[-1]+6*k)
+    k+= 1
+print(k)
