a11b9d67709800cef963d0f70297f9b7e2e71117
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-07-26 13:03:47 +0900

    [Silver V] Title: 종이자르기, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/2628. 종이자르기/종이자르기.py
+++ 백준/Silver/2628. 종이자르기/종이자르기.py
@@ -1,24 +1,26 @@
 w, h = map(int, input().split())
-
 n = int(input())
-w_lst = [0, w]
-h_lst = [0, h]
-cut_lst = []
-for _ in range(n):
-    cut = list(map(int, input().split()))
-    if cut[0] == 1:
-        w_lst.append(cut[1])
+
+w_lst = [0,w]
+h_lst = [0,h]
+for i in range(n):
+    way, k = map(int, input().split())
+    if way == 0:
+        h_lst.append(k)
     else:
-        h_lst.append(cut[1])
+        w_lst.append(k)
+
 w_lst.sort()
 h_lst.sort()
-w_max = 0
-h_max = 0
 
-for i in range(len(w_lst)-1):
-    w_max = max(w_lst[i+1]-w_lst[i], w_max)
-for i in range(len(h_lst)-1):
-    h_max = max(h_lst[i+1]-h_lst[i], h_max)
+max_w = 0
+max_h = 0
+for i in range(1, len(w_lst)):
+    if w_lst[i]-w_lst[i-1]>max_w:
+        max_w = w_lst[i]-w_lst[i-1]
+for j in range(1, len(h_lst)):
+    if h_lst[j]-h_lst[j-1]>max_h:
+        max_h = h_lst[j]-h_lst[j-1]
 
+print(max_w*max_h)
 
-print(w_max * h_max)
