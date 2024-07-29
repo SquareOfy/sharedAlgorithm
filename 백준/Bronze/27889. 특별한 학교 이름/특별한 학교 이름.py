db16f999d26905a53007fc398aa072a5bba581b5
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-07-29 20:10:41 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,6 @@
+d = {"NLCS":"North London Collegiate School",
+     "BHA": "Branksome Hall Asia",
+     "KIS" : "Korea International School",
+     "SJA": "St. Johnsbury Academy"}
+string = input()
+print(d[string])
