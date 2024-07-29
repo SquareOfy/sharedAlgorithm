1b74352f915ac3f2f7c0fe608c15d6caefce1f5e
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-07-29 19:29:39 +0900

    [Silver IV] Title: 빙고, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/2578. 빙고/빙고.py
+++ 백준/Silver/2578. 빙고/빙고.py
@@ -1,72 +1,62 @@
-my_arr = [list(map(int, input().split())) for _ in range(5)]
-called_arr = [list(map(int, input().split())) for _ in range(5)]
+my_bingo = [list(map(int, input().split())) for _ in range(5)]
+called_bingo = [list(map(int, input().split())) for _ in range(5)]
 
 
-def check_bingo(arr, r, c):
-    answer = 0
-    cnt1 = 0
-    # 가로 체크
-    for k in range(5):
-        if arr[r][k] == 0:
-            cnt1 += 1
-    if cnt1 == 5:
-        answer += 1
-    cnt1 = 0
-    for k in range(5):
-        if arr[k][c] == 0:
-            cnt1 += 1
-    if cnt1 == 5:
-        answer += 1
-
-    # 대각선 확인
-    if r == c:
-        cnt1 = 0
-        for i in range(5):
-            if arr[i][i] == 0:
-                cnt1 += 1
-
-        if cnt1 == 5:
-            answer += 1
-
-    if 4 - r == c:
-        cnt2 = 0
-        for i in range(5):
-            if arr[4 - i][i] == 0:
-                cnt2 += 1
-
-        if cnt2 == 5:
-            answer += 1
-
-    return answer
-
-
-def call_bingo():
-    global cnt
-
-    bingo_cnt = 0
-    for i in range(5):
-        if bingo_cnt == 3:
-            break
-        for j in range(5):
-            cnt += 1
-            if bingo_cnt == 3:
-                break
-            call = called_arr[i][j]
-            point = delete_number(call)
-
-            bingo_cnt += check_bingo(my_arr, point[0], point[1])
-            if bingo_cnt >= 3:
-                return
-
-
-def delete_number(call):
+def erase(i):
     for r in range(5):
         for c in range(5):
-            if my_arr[r][c] == call:
-                my_arr[r][c] = 0
-                return [r, c]
+            if my_bingo[r][c] == i:
+                my_bingo[r][c] = 0
+                return r, c
 
 
+def check_bingo(i):
+    global bingo
+    r, c = erase(i)
+
+    h = 0
+    w = 0
+    for k in range(5):
+        if my_bingo[r][k] == 0:
+            w += 1
+        if my_bingo[k][c] == 0:
+            h += 1
+    if w == 5:
+        bingo += 1
+    if h == 5:
+        bingo += 1
+    cross1 = 0
+    if r == c:
+        for k in range(5):
+            if my_bingo[k][k] == 0:
+                cross1 += 1
+            else:
+                break
+    if cross1 == 5:
+        bingo += 1
+
+    cross2 = 0
+    if r == 4 - c:
+        for k in range(5):
+            if my_bingo[k][4 - k] == 0:
+                cross2 += 1
+            else:
+                break
+    if cross2 == 5:
+        bingo += 1
+
+
+bingo = 0
 cnt = 0
-call_bingo()
-print(cnt)
+
+
+def pro():
+    global cnt
+    for lst in called_bingo:
+        for i in lst:
+            cnt += 1
+            check_bingo(i)
+            if bingo >= 3:
+                print(cnt)
+                return
+pro()
