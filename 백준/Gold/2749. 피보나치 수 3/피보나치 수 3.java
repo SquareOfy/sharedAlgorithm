761e1c58e156ec8e3bb0a286a799c0f50990d893
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-07-29 12:50:42 +0900

    [Gold II] Title: 피보나치 수 3, Time: 128 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.java
+++ 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.java
@@ -2,52 +2,45 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
 
-    static long N;
-    static final int mod = 1000000;
-    static final long[][] fiboArr = new long[][] {{1,1},{1,0}};
+public class Main {
+    static int mod = 1000000;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(br.readLine());
+        long[][] matrix = {{1,1},{1,0}};
 
-        N = Long.parseLong(br.readLine());
+        long[][] answer = pow(n, matrix);
+        System.out.println(answer[0][1]%mod);
 
-
-
-        long[][] result = pow(fiboArr, N);
-        System.out.println(result[0][1]%mod);
     }
 
-
-
-
-    public static long[][] pow(long[][] matrix, long times){
-
-        if(times ==1) return matrix;
-
-        long[][] result = pow(matrix, times/2);
-
-
-        result = multiplyMatrix(result, result);
-
-        if(times%2 ==0) return result;
-        return multiplyMatrix(result, matrix);
-    }
-    public static long[][] multiplyMatrix(long[][] matrix1, long[][] matrix2) {
-
+    static long[][] multi(long[][] m1, long[][] m2){
         long[][] result = new long[2][2];
 
-
-        for (int n = 0; n < 2; n++) {
-            for (int m = 0; m < 2; m++) {
-                for (int i = 0; i < 2; i++) {
-                    result[n][m] += (matrix1[n][i] * matrix2[i][m]) % mod;
-                    result[n][m] %= mod;
+        for(int r=0; r<2; r++){
+            for(int c=0; c<2; c++) {
+                for (int k = 0; k < 2; k++) {
+                    result[r][c] += (m1[r][k] * m2[k][c])%mod;
                 }
+                result[r][c] %= mod;
             }
         }
-        return result;
 
+        return result;
+    }
+    static long[][] pow(long n, long[][] matrix){
+        if(n==1){
+            return matrix;
+        }
+
+        long[][] result = pow(n/2, matrix);
+        result = multi(result, result);
+        if(n%2 ==0){
+            return result;
+        }
+        return multi(result, matrix);
 
     }
 }
