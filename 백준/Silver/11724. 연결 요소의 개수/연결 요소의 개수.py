c86f957b01e166b4ad15e409e9b854b7779ea148
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-08-01 10:28:46 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 488 ms, Memory: 173480 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,38 +1,21 @@
 n, m = map(int, input().split())
 edges = [[] for i in range(n + 1)]
-p = [i for i in range(n + 1)]
-
-
-def find_parent(x):
-    if p[x] == x:
-        return x
-    p[x] = find_parent(p[x])
-    return p[x]
-
-
-def union(x, y):
-    x = find_parent(x)
-    y = find_parent(y)
-    if x < y:
-        p[y] = x
-    else:
-        p[x] = y
-
+def dfs(i):
+    visited[i] = 1
+    for e in edges[i]:
+        if not visited[e]:
+            dfs(e)
 
 for i in range(m):
     u, v = map(int, input().split())
     edges[u].append(v)
     edges[v].append(u)
 
-for i in range(n):
-    for j in edges[i]:
-        union(i, j)
 
-visited = [0] * (n + 1)
+visited = [0]*(n+1)
 cnt = 0
-for i in range(1, n + 1):
-    find_parent(i)
-    if not visited[p[i]]:
+for i in range(1, n+1):
+    if not visited[i]:
         cnt += 1
-        visited[p[i]] = 1
+        dfs(i)
 print(cnt)
