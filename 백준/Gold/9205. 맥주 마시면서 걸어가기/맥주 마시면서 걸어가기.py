5ea5724e4f4b5428e88e51e42545fe899c466e47
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-08-01 15:15:20 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 196 ms, Memory: 114152 KB -BaekjoonHub

--- 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
@@ -1,27 +1,36 @@
 from collections import deque
 t = int(input())
 
+def dist(x,y, i, j):
+    return abs(x-i) + abs(y-j)
+
+
 for tc in range(t):
     n = int(input())
-    q = deque([])
-    visited = [0] *(n+2)
-    points = [list(map(int, input().split())) for _ in range(n+2)]
-
+    si, sj = map(int, input().split())
+    plst = [list(map(int, input().split())) for _ in range(n)]
+    ei, ej = map(int, input().split())
+    limit = 1000
     answer = "sad"
+    if dist(si,sj, ei,ej) <= limit:
+        print("happy")
+        continue
 
-    q.append(0)
-    visited[0] = 1
-    while len(q)>0:
+    q = deque([[si, sj]])
+    visited = [0]*n
+
+    while q:
         now = q.popleft()
-        if now == n+1:
+        r = now[0]
+        c = now[1]
+        if dist(r, c, ei, ej) <= limit:
             answer = "happy"
             break
-        for i in range(n+2):
-            if visited[i] == 0:
-                dist = abs(points[i][0]-points[now][0]) + abs(points[i][1]-points[now][1])
-                if dist > 1000:
-                    continue
-                visited[i] = 1
-                q.append(i)
 
+        for i in range(n):
+            pi = plst[i][0]
+            pj = plst[i][1]
+            if not visited[i] and dist(r, c, pi, pj)<=limit:
+                q.append(plst[i])
+                visited[i] = 1
     print(answer)
