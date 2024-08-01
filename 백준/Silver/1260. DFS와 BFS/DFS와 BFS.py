0cfcf05984fc123cd9bc7c9e2f1271df6f18b61b
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-08-01 09:34:43 +0900

    [Silver II] Title: DFS와 BFS, Time: 172 ms, Memory: 127052 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,50 +1,42 @@
 from collections import deque
+import sys
+sys.setrecursionlimit(10000)
 
+def dfs(i):
+    visited[i] = 1
+    dfs_answer.append(i)
+
+    for k in edges[i]:
+        if not visited[k]:
+            dfs(k)
 def bfs():
-    q = deque([])
+    q = deque([v])
     visited = [0]*(n+1)
-    q.append(v)
     visited[v] = 1
 
-    while len(q) != 0:
+    while q:
         now = q.popleft()
         bfs_answer.append(now)
-        for i in connected[now]:
-            if not visited[i]:
-                visited[i] = 1
-                q.append(i)
+
+        for k in edges[now]:
+            if not visited[k]:
+                visited[k] = 1
+                q.append(k)
+
 
 n, m, v = map(int, input().split())
-
-
-connected = [[] for _ in range(n+1)]
-
+edges = [[] for _ in range(n+1)]
 for i in range(m):
     st, ed = map(int, input().split())
-    connected[st].append(ed)
-    connected[ed].append(st)
+    edges[st].append(ed)
+    edges[ed].append(st)
 for i in range(n+1):
-    connected[i].sort(reverse=True)
+    edges[i].sort()
 
-stk = [v]
-visited = [0]*(n+1)
 dfs_answer = []
 bfs_answer = []
-
-while stk:
-    cur = stk.pop()
-    if not visited[cur]:
-        visited[cur] = 1
-        dfs_answer.append(cur)
-        for i in connected[cur]:
-            if not visited[i]:
-                stk.append(i)
-
-for i in range(n+1):
-    connected[i].sort()
-
+visited = [0]*(n+1)
+dfs(v)
 bfs()
-
 print(*dfs_answer)
-
 print(*bfs_answer)
