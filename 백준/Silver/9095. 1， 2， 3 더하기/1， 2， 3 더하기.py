a5110fb9c10790f0dc8b2596e70bb736923557a1
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-08-02 19:43:24 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 120 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -0,0 +1,14 @@
+t = int(input())
+
+for tc in range(t):
+    n = int(input())
+    dp = [0] * 11
+    dp[1] = 1
+    if n>=2:
+        dp[2] = 2
+    if n>=3:
+        dp[3] = 4
+
+    for i in range(4, n+1):
+        dp[i] = dp[i-1]+dp[i-2]+dp[i-3]
+    print(dp[n])
