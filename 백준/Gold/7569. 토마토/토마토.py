c005ce3628efb7dbcaa1f54d31bc84aaa816fdee
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-08-02 10:21:43 +0900

    [Gold V] Title: 토마토, Time: 648 ms, Memory: 205460 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,54 +1,38 @@
 from collections import deque
+m,n,h = map(int, input().split())
 
-m, n, h = map(int, input().split())
-
-#h - n - m 순
-#위칸 상 좌 아래칸 하 우
-d = ((-1, 0, 0), (0, -1, 0), (0, 0,-1), (1, 0, 0), (0,1,0), (0,0,1))
-
-#1은 익은 토마토 0은 익지 않음 -1은 토마토 없음
-#1인 곳이 상하 좌우 앞 뒤 여섯 방향의 토마토로 전파
-arr = [[list(map(int, input().split())) for _ in range(n)] for _ in range(h)]
-
-#방문배열 1이면 방문 함
-# visited = [[[0]*m for _ in range (n)] for _ in range(h)]
-
-
-def check_tomato():
-    for a in arr:
-        for b in a:
-            for c in b:
-                if c == 0:
-                    return False
-    return True
+arr = [[list(map(int, input().split())) for i in range(n)] for j in range(h)]
 
 q = deque([])
-for k in range(h):
-    for i in range(n):
-        for j in range(m):
-            if arr[k][i][j] == 1:
-                q.append([k, i, j, 0])
-
-answer = 0
-
-while len(q)!=0 :
-    now = q.popleft()
-    rank = now[3]
-    answer = rank
-    for i in d:
-        dw = now[0] + i[0]
-        du = now[1] + i[1]
-        dv = now[2] + i[2]
-        if dw<0 or du<0 or dv<0 or dw>=h or du>=n or dv>=m:
-            continue
-        if arr[dw][du][dv] == 0:
-            arr[dw][du][dv] = 1
-            q.append([dw, du, dv, rank+1])
-
-
-if check_tomato():
-    print(answer)
+cnt = 0 #익지 않은 토마토 개수
+for i in range(h):
+    for j in range(n):
+        for k in range(m):
+            if arr[i][j][k] == 1:
+                q.append([i,j,k, 0])
+            elif arr[i][j][k] == 0:
+                cnt+=1
+if cnt == 0:
+    print(0)
 else:
-    print(-1)
+    answer = 0
+    while q:
+        now = q.popleft();
+        k, r, c, rank = now
+        answer = rank
+        for d in (-1, 0, 0), (0, -1, 0), (0, 0, -1), (1, 0, 0), (0, 1, 0), (0, 0, 1):
+            dk = k+ d[0]
+            du = r+d[1]
+            dv = c+d[2]
+            if dk<0 or dk>=h or du<0 or du>=n or dv<0 or dv>=m:
+                continue
 
+            if arr[dk][du][dv] == 0 :
+                arr[dk][du][dv] = 1
+                q.append([dk, du, dv, rank+1])
+                cnt -= 1
 
+    if cnt == 0:
+        print(answer)
+    else:
+        print(-1)
