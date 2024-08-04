f25a9a08e6be307fcfcb9981a65ba348bb92f4cd
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-08-04 10:21:57 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 232 ms, Memory: 130652 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -1,14 +1,16 @@
+import sys
+input = sys.stdin.readline
 n, m = map(int, input().split())
 
 d = dict()
 pockets = [0]*(n+1)
 for i in range(1, n+1):
-    pockets[i] = input()
+    pockets[i] = input().rstrip()
     d[pockets[i]] = i
 
 for i in range(m):
-    q = input()
+    q = input().rstrip()
     if d.get(q) is not None:
-        print(d.get(q))
+        print(d[q])
     else:
         print(pockets[int(q)])
