278da8966c8e5c289f1a081da9630a5d21895d78
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-08-05 16:01:23 +0900

    [Silver I] Title: 트리 순회, Time: 84 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.py
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -1,6 +1,8 @@
 def pre_order(i):
-    if i < 0:
+    if i < 0:  #'.'일 땐 바로 끝내기
         return
+    #pre order 순 대로 함수 호출 및 lst append
+    #중위후위순회도 마찬가지
     pre_answer.append(change_alph(i))
     pre_order(tree[i][0])
     pre_order(tree[i][1])
@@ -21,11 +23,11 @@ def post_order(i):
     post_order(tree[i][1])
     post_answer.append(change_alph(i))
 
-
+#알파벳을 배열 idx로 변환
 def change_idx(a):
     return ord(a) - ord('A')
 
-
+#배열 idx를 알파벳으로 변환
 def change_alph(a):
     return chr(a + ord('A'))
 
@@ -35,7 +37,11 @@ m = ord('A')
 tree = [0] * (n + 1)
 
 for i in range(n):
+    #idx로 변환하여 값 받기
     st, left, right = map(change_idx, input().split())
+    #tree에 자식들의 배열idx 값 저장
+    #tree node의 값은 따로 저장하지 않고 
+    #idx만 가지고 나중에 알파벳으로 변환해서 찾을 것
     tree[st] = [left, right]
 
 pre_answer = []
@@ -45,6 +51,7 @@ pre_order(0)
 in_order(0)
 post_order(0)
 
+#결과 출력
 print(''.join(pre_answer))
 print(''.join(in_answer))
 print(''.join(post_answer))
