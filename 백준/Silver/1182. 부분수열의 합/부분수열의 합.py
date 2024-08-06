624e81202c69eef929b4585b9156fcf0bdd19aea
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-08-06 19:27:50 +0900

    [Silver II] Title: 부분수열의 합, Time: 220 ms, Memory: 112284 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,20 +1,20 @@
 n, s = map(int, input().split())
-seq = list(map(int, input().split()))
+lst = list(map(int, input().split()))
 
-answer = 0
-
-
-def brute_force(idx, num_sum):
+#level은 lst의 인덱스. 0~n-1까지 dfs로 돌며 
+# 해당 값을 더한 경우와 안 더한 경우 각각 호출 
+#cnt는 부분수열의 크기(한개도 안 고른 경우를 배제하기 위함)
+#selected_sum에 이번 level에서 고른 수를 더해서 넘기기
+def dfs(level, cnt, selected_sum):
     global answer
-
-    if idx == n:
+    if level == n:
+        if cnt>0 and selected_sum == s:
+            answer+=1
         return
 
-    brute_force(idx+1, num_sum+seq[idx])
-    if(num_sum+seq[idx]==s): answer+= 1
-    brute_force(idx+1, num_sum)
+    dfs(level + 1, cnt+1, selected_sum + lst[level]) #이번것 골랐을 때
+    dfs(level + 1, cnt, selected_sum) #안골랐을 때
 
-
-brute_force(0, 0)
+answer = 0
+dfs(0, 0, 0)
 print(answer)
-
