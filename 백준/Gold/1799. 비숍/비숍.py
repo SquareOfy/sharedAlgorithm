63a4e3177603a7291794c44c74fe8e20e0815359
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-08-09 19:20:08 +0900

    [Gold I] Title: 비숍, Time: 988 ms, Memory: 120696 KB -BaekjoonHub

--- 백준/Gold/1799. 비숍/비숍.py
+++ 백준/Gold/1799. 비숍/비숍.py
@@ -3,50 +3,44 @@ arr = [list(map(int, input().split())) for _ in range(n)]
 other_diagonal = [0]*(2*n-1)
 
 
-def find_diagonal_idx(i, j):
-    return n-1+j-i
-
-
 def dfs(level, cnt):
     global answer
-    if 2*n-1-level < answer-cnt:
+    if answer == 2*n - 2:
         return
-    if answer == 2*n-2:
+    if cnt == 2*n-2:
+        answer = cnt
+        return
+    if cnt + (2*n - level) < answer:
         return
     if level == 2*n:
         answer = max(answer, cnt)
-        # print(selected)
-
         return
-    l = abs(abs(level+1-n)-n)
 
-    flag = False
+    if level < n:
+        l = level + 1
+    else:
+        l = 2*n - (level + 1)
+
+    flag = True
     for i in range(l):
-        r= -1
-        c= -1
-        if level <=n-1:
-            r = level-i
-            c = l-r-1
+        if level < n:
+            r = level - i
+            c = i
         else:
-            r = n-1-i
-            c = level-r
-
+            r = (n - 1) - i
+            c = level -r
         if arr[r][c] == 1:
-            idx = find_diagonal_idx(r, c)
-
+            idx = (n - 1) + (c - r)
             if other_diagonal[idx]:
                 continue
+            flag = False
             other_diagonal[idx] = 1
             dfs(level+1, cnt+1)
             other_diagonal[idx] = 0
-            flag = True
-
-    if not flag:
+    if flag:
         dfs(level+1, cnt) #그 줄에서 안고를 때??
+
 
 answer = 0
 dfs(0,0)
 print(answer)
-
-
-
