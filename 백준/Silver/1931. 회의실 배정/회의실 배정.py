ad959e3752f09ed7b2a5ce447592c70b99c01db3
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-08-09 09:50:56 +0900

    [Silver I] Title: 회의실 배정, Time: 352 ms, Memory: 127316 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,13 +1,19 @@
 n = int(input())
 
-meetings = [list(map(int, input().split())) for _ in range(n)]
+arr = [list(map(int, input().split())) for i in range(n)]
 
-meetings.sort(key=lambda x:(x[1], x[0]))
+#회의실을 최대한 많이 선택하기 위해
+#끝나는 시간 기준으로 오름차순 정렬
+#빨리 끝나는 회의실 우선 선택하고, 그 회의실 이후에 시작하는 회의실 중
+#또 가장 빨리 끝나는 회의실 선택하면 됨
 
-cnt = 0
+arr.sort(key=lambda x : (x[1], x[0]))
+
 last = 0
+answer = 0
 for i in range(n):
-    if meetings[i][0]>=last:
-        cnt+=1
-        last = meetings[i][1]
-print(cnt)
+    if last<=arr[i][0]: #직전에 선택한 회의 종료시간 이후에 회의 시작하면 선택
+        last = arr[i][1]
+        answer += 1
+
+print(answer)
