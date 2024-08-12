85694e7ed27894fc7263dc4888a8a68708583d7b
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-08-12 14:43:13 +0900

    [Bronze II] Title: 수 정렬하기, Time: 108 ms, Memory: 110644 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,49 +1,66 @@
-def quick_sort(st, ed):
-    if st >= ed:
+def merge_sort(left, right):
+    #정렬할 배열이 한자리일 땐, answer 그 자리에 그 값 저장하고 정렬 끝
+    if left >= right:
+        answer[left] = lst[left]
         return
-    
-    pivot = st #pivot index 제일 왼쪽 값
-    left = st + 1 #pivot 바로 오른쪽 idx가 left
-    right = ed #제일 오른쪽이 right로 시작
+    mid = (left + right) // 2
+    #가운데 기준으로 왼쪽 배열 병합정렬시키기
+    merge_sort(left, mid)
+    #오른쪽 배열 병합정렬 시키기
+    merge_sort(mid + 1, right)
+    #양 쪽 병합정렬 완료했으면 두 배열 비교해서 병합
+    merge(left, right)
 
-    #left, right가 같아질 때까지 아래 반복
-    while left < right: 
-        #left와 right를 찾아서 둘의 자리를 바꿀 것 
-        #자리를 바꾼 후의 left자리가 pivot보다 작고 right가 pivot보다 커야하므로
-        #자리를 바꿀 대상을 찾기 위해 left값(배열값) pivot보다 커질 때,
-        #right값이 pivot보다 작아지는 순간을 찾아 자리 바꿀 것
+
+
+def merge(left, right):
+    #가운데 값을 기준으로 배열 나누기
+    mid = (left + right) // 2
+    #가장 왼쪽값부터 작은 순으로 정답배열 채워갈 것
+    idx = left
+
+    
+    i = left        #왼쪽 배열 index
+    j = mid + 1     #오른쪽 배열 index
+
+    #왼쪽 오른쪽 중 어느 한 쪽 다 소진할 때까지 answer배열로 작은 값 옮기기
+    while i < mid + 1 and j <= right:
         
-        #pivot 값보다 커지는 순간이 left로 잡기
-        while left < ed and lst[left] <= lst[pivot]:
-            left += 1
-        #pivot 값보다 작아지는 순간이 right
-        while right > st and lst[right] >= lst[pivot]:
-            right -= 1
-            
-        #아직 둘이 같지 않다면 자리 바꾸기
-        if left<right:
-            lst[left], lst[right] = lst[right], lst[left]
-    #left와 right를 자리 바꿔가며 pivot 값을 기준으로 작은값, 큰값 분류를 마쳤다면
-    #pivot과 자리 바꾸기
-    #pivot보다 작은 애들 중 가장 오른쪽에 있는 idx 값과 자리를 바꿔야 하므로 
-    #left의 배열 값을 본다.
-    #위의 while문에서 left를 +1하며, lst[left]값이 lst[pivot]보다 커질 수 있으므로
-    #그럴 땐 left-1 자리에 있는 값과 pivot자리를 바꿔주고 
-    #바뀐 pivot의 위치를 pivot변수에 기억한다.(다음 좌, 우 배열을 퀵정렬 하기 위해서)
-    if lst[left] > lst[pivot]:
-        lst[left - 1], lst[pivot] = lst[pivot], lst[left - 1]
-        pivot = left - 1
-    else:
-        lst[left], lst[pivot] = lst[pivot], lst[left]
-        pivot = left
+        #왼쪽 값이 더 작으면
+        if lst[i] < lst[j]: 
+            answer[idx] = lst[i]    #왼쪽 채워주고
+            i += 1                  #왼쪽 index +1
 
+        else:                       #오른쪽이 더 작으면 오른쪽 채우고
+            answer[idx] = lst[j]
+            j += 1                  #index + 1
+        idx += 1                    #answer배열 가리키는 index +1
+        
+    #위 while문에서 왼쪽 혹은 오른쪽을 다 소진했다면 
+    #어느 한 쪽은 아직 숫자가 남아있다.
+    #따라서 어디가 남았냐에 따라 두 while문 중 하나만 동작하게 된다
+
+    # 왼쪽배열을 가리키던 index가 아직 끝값에 도달 못한다면
+    # 아래 조건문이 True가 되어 동작
+    while i <= mid:
+        answer[idx] = lst[i]    #i가 갈 수 있는 끝까지 가며 answer채우기
+        i += 1
+        idx += 1
+        
+    #반대로 오른쪽에 수가 남으면 아래가 동작
+    while j <= right:
+        answer[idx] = lst[j]
+        j += 1
+        idx += 1
     
-    quick_sort(st, pivot - 1) #pivot을 기준으로 왼쪽 배열 정렬하기
-    quick_sort(pivot + 1, ed) #pivot을 기준으로 오른쪽 배열 정렬하기
-
+    #answer을 다 채웠다면 다음 배열 정렬하러 갈텐데, 
+    #위에서 계속 lst에 있는 값을 비교하며 answer을 채워나갔다.
+    #따라서 지금까지 정렬된 answer값을 lst에 갱신해줘야
+    #다음 정렬을 할 때, 지금까지 정렬한 결과값을 활용하여 정상적으로 동작할 수 있다.
+    lst[left:right+1] = answer[left:right+1]
 
 n = int(input())
 lst = [int(input()) for _ in range(n)]
-quick_sort(0, n - 1)
-print(*lst, sep="\n")
-
+answer = [0] * n
+merge_sort(0, n-1)
+print(*answer, sep="\n")
