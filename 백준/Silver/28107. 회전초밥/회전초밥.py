c48efd8ff6090ba32cfbeec0ac2b6c18ba918650
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-08-12 19:13:22 +0900

    [Silver I] Title: 회전초밥, Time: 416 ms, Memory: 163192 KB -BaekjoonHub

--- 백준/Silver/28107. 회전초밥/회전초밥.py
+++ 백준/Silver/28107. 회전초밥/회전초밥.py
@@ -2,25 +2,14 @@ import heapq
 n, m = map(int, input().split())
 
 arr = [list(map(int, input().split())) for _ in range(n)]
-
 sushi = list(map(int, input().split()))
-hq = []
-answer = [0]*n
-sushi.sort()
+order = [[] for _ in range(200_001)]
+answer = [0] * n
+
 for i in range(n):
     for j in range(1, len(arr[i])):
-        heapq.heappush(hq, (arr[i][j], i))
-
-for i in range(m):
-    while 1:
-        if not hq:
-            break
-
-        p = heapq.heappop(hq)
-        if p[0] > sushi[i]:
-            heapq.heappush(hq, p)
-            break
-        if p[0] == sushi[i]:
-            answer[p[1]] += 1
-            break
+        heapq.heappush(order[arr[i][j]], i)
+for s in sushi:
+    if order[s]:
+        answer[heapq.heappop(order[s])]+=1
 print(*answer)
