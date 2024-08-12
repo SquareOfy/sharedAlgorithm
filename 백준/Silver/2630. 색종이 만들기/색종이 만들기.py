98e1732548d0103f74f3970bc7e511bf50df96b6
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-08-12 16:22:57 +0900

    [Silver II] Title: 색종이 만들기, Time: 148 ms, Memory: 112220 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -1,43 +1,27 @@
-n = int(input())
-
-arr = [list(map(int, input().split())) for _ in range(n)]
-blue = 0
-white = 0
-
-
-def check_same_color(l, r, c):
+def check_color(r, c, size):
+    if size==1:
+        return arr[r][c]
     color = arr[r][c]
-    for i in range(r, r + l):
-        for j in range(c, c + l):
-            if arr[i][j] != color:
+    for i in range(size):
+        for j in range(size):
+            if arr[r+i][c+j] != color:
                 return -1
     return color
 
 
-def cut_paper(l, r, c):
-    global blue
-    global white
-    if l == 1:
-        if arr[r][c] == 0:
-            white += 1
-        else:
-            blue += 1
+def make_color_paper(r, c, size):
+    color = check_color(r, c, size)
+    if color != -1:
+        answer[color] += 1
         return
-    result = check_same_color(l, r, c)
-
-    if result == 0:
-        white += 1
-        return
-    if result == 1:
-        blue += 1
-        return
-    next = l // 2
-    cut_paper(next, r, c)
-    cut_paper(next, r + next, c)
-    cut_paper(next, r, c + next)
-    cut_paper(next, r + next, c + next)
+    size //= 2
+    for i in range(2):
+        for j in range(2):
+            make_color_paper(r + i*size, c + j*size, size)
 
 
-cut_paper(n, 0, 0)
-print(white)
-print(blue)
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+answer = [0] * 2
+make_color_paper(0, 0, n)
+print(*answer, sep="\n")
