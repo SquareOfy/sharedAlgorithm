71336f35e55e363a352ee848177efd23a87f1d9d
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-08-14 20:18:46 +0900

    [Silver I] Title: Z, Time: 96 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.py
+++ 백준/Silver/1074. Z/Z.py
@@ -1,31 +1,30 @@
-n, r, c = map(int, input().split())
-
-cnt = 0
-answer = 0
-
-
-def zigzag(l, i, j):
-    global cnt
-    global answer
-    if l == 1:
-        if i == r and j == c:
-            answer = cnt
-        cnt += 1
+def split_recursion(l, i, j):
+    global cnt,flag
+    if flag:
         return
+    if l == 1:
+        if i==r and j==c:
+            flag = True
+            return
+        cnt+=1
 
+        return
     size = l//2
+    #제1사분면
     if i<=r<i+size and j<=c<j+size:
-        zigzag(size, i, j)
-    elif i<=r<i+size and j+size<=c<j+size*2:
-        cnt+= size*size
-        zigzag(size, i, j+size)
-    elif i+size<=r<i+size*2 and j<=c<j+size:
-        cnt+= size*size*2
-        zigzag(size, i+size, j)
+        split_recursion(size, i, j)
+    elif i<=r<i+size and j+size<=c:
+        cnt+=size*size
+        split_recursion(size, i, j+size)
+    elif i+size<=r and j<=c<j+size:
+        cnt+=size*size*2
+        split_recursion(size, i+size, j)
     else:
-        cnt+= size*size*3
-        zigzag(size, i+size, j+size)
+        cnt+=size*size*3
+        split_recursion(size, i+size, j+size)
 
-
-zigzag(2 ** n, 0, 0)
-print(answer)
+n, r, c = map(int, input().split())
+cnt = 0
+flag = False
+split_recursion(2**n, 0, 0)
+print(cnt)
