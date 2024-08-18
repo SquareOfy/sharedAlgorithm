c989cdbaf303746e96059ef5b68b09249c76693d
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-08-18 17:58:57 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 100 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -1,14 +1,14 @@
 n = int(input())
-seq = list(map(int, input().split()))
+lst = list(map(int, input().split()))
 
-dp = [1]*n
+answer = [lst[0]]
 
-#dp를 채울 index
 for i in range(1, n):
-    for j in range(i): #내 앞에 있는 수열들 탐색
-        if seq[j] < seq[i]: #나보다 내 앞에 작은 값이 있으면
-            #그 값까지 가장 긴 증가하는 부분수열 길이에 +1한 값과
-            #현재의 dp값 중 큰 값으로 갱신
-            dp[i] = max(dp[i], dp[j]+1) #
-print(max(dp))
-
+    if answer[-1] < lst[i]:
+        answer.append(lst[i])
+    else:
+        for j in range(len(answer)):
+            if answer[j] >= lst[i]:
+                answer[j] = lst[i]
+                break
+print(len(answer))
