127b2e9dbdedb9ec0b7566f46824c5fc6ab3f9ab
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-08-19 16:29:03 +0900

    [Silver III] Title: 달팽이, Time: 228 ms, Memory: 119948 KB -BaekjoonHub

--- 백준/Silver/1913. 달팽이/달팽이.py
+++ 백준/Silver/1913. 달팽이/달팽이.py
@@ -1,57 +1,49 @@
+def move(d):
+    global r,c, cnt, num
+    for k in range(cnt):
+        r += d[0]
+        c += d[1]
+        if r>=n or r<0 or c<0 or c>=n:
+            return False
+        arr[r][c] = num
+        if num == t:
+            answer[0] =r+1
+            answer[1] = c+1
+        num+=1
+        if k!=cnt-1:
+            if d[0] != 0 and r==0:
+                return False
+    if d in ((0, 1), (0, -1)):
+        cnt += 1
+    return True
 n = int(input())
-m = int(input())
+t = int(input())
 
-arr = [[0]*n for _ in range(n)]
+answer = [-1, -1]
+arr = [[0] * n for _ in range(n)]
 
 r = n//2
 c = n//2
-
-turn = 1
-cnt = 1
 arr[r][c] = 1
-result = [r+1,c+1]
-idx = 2
-def write_num():
-    global arr, result, idx
+cnt = 1 #이동할 칸수
+num = 2
+if t ==1:
+    answer = [r+1, c+1]
 
-    arr[r][c] = idx
+while 1:
+    #위로 이동
+    if not move((-1, 0)):
+        break
+    #오른쪽 이동 : 이동 후 이동하는 칸수 증가
+    if not move((0, 1)):
+        break
+    #아래쪽 이동
+    if not move((1,0)):
+        break
+    #왼쪽이동 : 이동 후 이동하는 칸 수 증가
+    if not move((0, -1)):
+        break
 
-    if idx == m:
-        result = [r+1, c+1]
-    idx+=1
-
-
-
-while idx < n*n :
-    if cnt % 4 == 1 : #up
-        for i in range(turn):
-            if idx > n*n:
-                break
-            r -= 1
-            write_num()
-        cnt+=1
-
-    elif cnt % 4 == 2 : #right
-        for i in range(turn):
-            c += 1
-            write_num()
-        cnt += 1
-        turn += 1
-
-    elif cnt % 4 == 3 : #down
-        for i in range(turn):
-            r += 1
-            write_num()
-        cnt += 1
-
-    elif cnt % 4 == 0: #left
-        for i in range(turn):
-            c -= 1
-            write_num()
-        cnt += 1
-        turn += 1
-
-
-for i in arr:
-    print(*i)
-print(*result)
+for i in range(n):
+    print(*arr[i])
+print(*answer)
