c75b6ddb62d6e2496eb646ec48b769ddee713a84
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-08-21 16:30:40 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
@@ -1,32 +1,28 @@
 n = int(input())
-switches = list(map(int, input().split()))
-
+lst = [-1]+list(map(int, input().split()))
 m = int(input())
-students = []
 for _ in range(m):
-    students.append(list(map(int, input().split())))
+    gender, num = map(int, input().split())
 
-for p in students:
-    num = p[1]
-    if p[0] == 1:
-        for i in range(num-1, n, num):
-            switches[i] = (switches[i] + 1) % 2
-
-    else:
-        switches[num-1] = (switches[num-1] + 1) % 2
-        j = 1
-        while 1:
-            if num-1 + j >= n or num-1 - j < 0: break
-            if switches[num-1+j] != switches[num-1-j]: break
-            switches[num-1+j] = (switches[num-1+j]+1) % 2
-            switches[num-1-j] = (switches[num-1-j]+1) % 2
-            j += 1
-
-
-if len(switches) <= 20: print(*switches)
+    if gender == 1: #남
+        for i in range(num, n+1, num):
+            lst[i] += 1
+            lst[i] %= 2
+    if gender == 2:
+        lst[num] = (lst[num]+1)%2
+        left = num-1
+        right = num+1
+        while left>=1 and right<=n and lst[left]==lst[right]:
+            lst[left] = (lst[left]+1)%2
+            lst[right] = (lst[right]+1)%2
+            left -= 1
+            right += 1
+if n<=20:
+    print(*lst[1:])
 else:
-    for i in range(len(switches)//20):
-        print(*switches[20*i:20*(i+1)])
-
-    if len(switches) % 20 != 0:
-        print(*switches[20*(len(switches)//20):])
+    idx = 1
+    for i in range(n//20):
+        print(*lst[idx:idx+20])
+        idx +=20
+    if n%20!=0:
+        print(*lst[idx:])
