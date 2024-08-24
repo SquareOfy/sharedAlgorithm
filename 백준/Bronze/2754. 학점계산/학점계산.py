3ab0cef8854e8f16bb84d37fa7f2e3e43c70933e
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-08-24 15:53:57 +0900

    [Bronze III] Title: 학점계산, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/2754. 학점계산/학점계산.py
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -1,22 +1,3 @@
-import sys
-
-input = sys.stdin.readline
-
-grade_dic = dict()
-grade_dic['A+'] = 4.3
-grade_dic['A0'] = 4.0
-grade_dic['A-'] = 3.7
-grade_dic['B+'] = 3.3
-grade_dic['B0'] = 3.0
-grade_dic['B-'] = 2.7
-grade_dic['C+'] = 2.3
-grade_dic['C0'] = 2.0
-grade_dic['C-'] = 1.7
-grade_dic['D+'] = 1.3
-grade_dic['D0'] = 1.0
-grade_dic['D-'] = 0.7
-grade_dic['F'] = 0.0
-
-grade = input().rstrip()
-
-print(grade_dic[grade])
+grade = {'A+':4.3, 'A0':4.0, 'A-':3.7, 'B+':3.3, 'B0':3.0, 'B-':2.7, \
+         'C+':2.3, 'C0':2.0, 'C-':1.7, 'D+':1.3, 'D0':1.0, 'D-':0.7, 'F':0.0}
+print(grade[input()])
