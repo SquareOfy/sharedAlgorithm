79bbae4017bc3cde9293151876047aa6adc472f1
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-08-24 14:03:57 +0900

    [Bronze V] Title: 윤년, Time: 92 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.py
+++ 백준/Bronze/2753. 윤년/윤년.py
@@ -1,9 +1,5 @@
-import sys
-
-input = sys.stdin.readline
-
-year = int(input())
-
-if year%400==0 or (year%4==0 and year%100!=0):
+y = int(input())
+if (y%4 ==0 and y%100!=0) or y%400==0:
     print(1)
-else: print(0)
+else:
+    print(0)
