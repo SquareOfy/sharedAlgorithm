e31e6bbdd51f45935cb697cbf58071efe7cadaa0
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-08-24 16:08:28 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 92 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
@@ -1,16 +1,6 @@
-import sys
+word = input()
 
-input = sys.stdin.readline
-
-s = list(input().rstrip())
-
-l = len(s)
-
-result = 1
-for i in range(l//2):
-    if(s[i]!=s[l-i-1]):
-        result = 0
-        break
-
-
-print(result)
+if word[:len(word)//2] == word[-1:-(len(word)//2+1):-1]:
+    print(1)
+else:
+    print(0)
