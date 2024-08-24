693d4095fa71bf44c29e06feadb1d9cf3e983d81
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-08-24 14:07:48 +0900

    [Bronze IV] Title: 인공지능 시계, Time: 96 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/2530. 인공지능 시계/인공지능 시계.py
+++ 백준/Bronze/2530. 인공지능 시계/인공지능 시계.py
@@ -1,19 +1,13 @@
-import sys
+a, b, c = map(int, input().split())
+d = int(input())
+c = c+d
 
-input = sys.stdin.readline
-
-
-h, m, s = map(int, input().split())
-sec = int(input())
-
-s+= sec
-if(s>59):
-    m+= s//60
-    s%=60
-    if(m>59):
-        h += m//60
-        m%=60
-        if(h>23):
-            h %=24
-
-print(str(h)+' '+str(m)+' '+str(s))
+h, m = 0, 0
+if c>=60:
+    b += c//60
+    c %= 60
+    if b>=60:
+        a += b//60
+        b %= 60
+a%=24
+print(a, b, c)
