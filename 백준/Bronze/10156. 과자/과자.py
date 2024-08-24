8ac9ae68c5c7eef7d25da8c5a2b106c45d23a6c1
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-08-25 00:13:08 +0900

    [Bronze IV] Title: 과자, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/10156. 과자/과자.py
+++ 백준/Bronze/10156. 과자/과자.py
@@ -1,10 +1,6 @@
-import sys
-
-input = sys.stdin.readline
-
 k, n, m = map(int, input().split())
-
-result = k*n - m
-if(result<0): result =0
-
-print(result)
+snack = k*n
+if snack>m:
+    print(snack-m)
+else:
+    print(0)
