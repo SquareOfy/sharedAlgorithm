d5ca744caf21b5339efbe18880997f1fe074ea9e
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-09-07 21:18:40 +0900

    [Silver II] Title: 점프 점프, Time: 84 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/11060. 점프 점프/점프 점프.py
+++ 백준/Silver/11060. 점프 점프/점프 점프.py
@@ -1,21 +1,38 @@
-from collections import deque
-n = int(input())
-lst = list(map(int, input().split()))
-answer = -1
-q = deque([(0, 0)])
-visited = [0]*n
-visited[0] = 1
+N = int(input())
+A = list(map(int, input().split()))
+ans = idx = t = 0
 
 
-while q:
-    i, rank = q.popleft()
-    if i==n-1:
-        answer = rank
+while True:
+    t = A[idx]
+    if idx >= N - 1:
         break
-    for k in range(1, lst[i]+1):
-        if i+k >=n:
-            continue
-        if not visited[i+k]:
-            visited[i+k] = 1
-            q.append((i+k, rank+1))
-print(answer)
+    ans += 1
+    # 점프가능 칸수
+
+    # index()에 걸리지 않도록 0으로 바꿔주기
+    # A[idx] = 0
+
+    # 0이 가장 큰값 => 0에서 더 이동 불가면 오른쪽 끝 갈 수 없음
+    if t == 0:
+        ans = -1
+        break
+
+    if idx + t >= N - 1:
+        break
+    # 가장 오른쪽끝 인덱스 도달한다면 탈출
+
+
+    # 내 다음위치~이동가능 위치 중 가장 큰값으로 점프할거
+    mx = 0
+    cnt = idx
+    # 가장 큰값으로 이동
+    for i in range(idx + 1, idx + t + 1):
+        if i+A[i] >= mx:
+            idx= i
+            mx = i+A[i]
+        # cnt += 1
+        # if i == mx:
+        #     idx = cnt
+
+print(ans)
