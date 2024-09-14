2d388dc827d7cf410b46b6011fe3f927543aead5
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-09-14 09:32:05 +0900

    [Silver I] Title: 유성, Time: 2072 ms, Memory: 501544 KB -BaekjoonHub

---
+++ 백준/Silver/10703. 유성/유성.py
@@ -0,0 +1,41 @@
+"""
+5 6
+.XXXX.
+......
+...#..
+#..###
+######
+
+6 3
+.X.
+...
+.##
+..#
+#.#
+###
+
+"""
+n, m = map(int, input().split())
+
+arr = [list(input()) for _ in range(n)]
+
+down = n
+#내릴 수 있는 min 찾기
+for c in range(m):
+    cnt= 0
+    for r in range(n-2, -1, -1):
+        if arr[r][c]=='#':
+            cnt = 0
+        elif arr[r][c] == '.':
+            cnt+=1
+        elif arr[r][c]=='X':
+            down = min(down, cnt)
+            continue
+for c in range(m):
+    for r in range(n-2, -1, -1):
+        if arr[r][c]=='X':
+            arr[r][c] = '.'
+            arr[r+down][c] = 'X'
+
+for i in range(n):
+    print(*arr[i], sep="")
