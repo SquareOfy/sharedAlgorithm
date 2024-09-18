9d62a115db6eaa4eb89349fc18362cc8fae33e3d
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-09-18 10:33:11 +0900

    [Gold IV] Title: 드래곤 앤 던전, Time: 216 ms, Memory: 123576 KB -BaekjoonHub

--- 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.py
+++ 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.py
@@ -1,38 +1,14 @@
-def check(mid):
-    attack = M
-    hp = mid
-
-    for t, a, h in arr:
-        # print('-------------', t,a,h)
-        if t == 1: #몬스터
-
-            k = h // attack if h%attack==0 else h//attack+1
-            c = hp // a if hp%a==0 else hp//a+1
-
-            h %= attack
-            hp -= a*(k-1)
-
-            if k<=c:
-                continue
-            else:
-                return False
-        else:
-            attack+=a
-            hp = min(mid, hp+h)
-    return True
-
 N, M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
-
-left = 1
-right = 1_000_000**2*N
-answer = -1
-while left<=right:
-    mid = (left+right)//2
-    if check(mid):
-        answer = mid
-        right = mid-1
+answer = 0
+tmp =0
+for t, a, h in arr:
+    if t==1:
+        k = h//M+1 if h%M else h//M #몬스터 죽이기 위해 필요한 공격횟수
+        tmp += (k-1)*a #그 때 깎이는 내 hp
+        answer = max(tmp, answer)
     else:
-        left = mid+1
-
-print(answer)
+        answer = max(tmp, answer)
+        M+=a
+        tmp = max(tmp-h, 0)
+print(answer+1)
