56e632b4a04723e792c366be66083178aa460500
SquareOf_y <109445759+SquareOfy@users.noreply.github.com>
2024-10-03 14:34:19 +0900

    [Gold III] Title: 연구소 3, Time: 180 ms, Memory: 114700 KB -BaekjoonHub

--- 백준/Gold/17142. 연구소 3/연구소 3.py
+++ 백준/Gold/17142. 연구소 3/연구소 3.py
@@ -1,85 +1,82 @@
 """
-총 풀이시간 21분
+N×N 크기의 도시
+병원과 벽을 제외한 모든 지역에 바이러스
+M개의 병원을 적절히 고르기
+
+골라진 병원들을 시작으로 매 초마다 상하좌우로 인접한 지역 중 벽을 제외한 지역에
+백신이 공급되기 때문에 그 자리에 있던 바이러스는 사라지게 됩니다.
 
 
-처음 모든 바이러스 비활성 상태
-
-활성 바이러스 상하좌우 빈칸을 "!!!동시!!!" 복제
-복제에 1초 소요
-
-바이러스 M개 활성상태로 변경하려고 함
-활성 바이러스가 비활성 바이러스가 있는 칸으로 가면 비활성 => 활성 됨
-
-0 빈칸
-1 벽
-2 바이러스
+M개의 병원을 적절히 골라 바이러스를 전부 없애는데 걸리는 시간 중 최소 시간을 구하는 프로그램
 
 
+0 : 바이러스
+1 : 벽
+2 : 병원
 
-모든 빈칸에 바이러스를 퍼뜨리는 최소 시간 ...
+3≤N≤50
+1≤M≤10
 
-빈칸 개수 세고 바이러스 퍼질 때마다 -1 시켜서 0될때 시간 min 갱신하자 .
-
-
-바이러스 M개 고르기 2500 C 10 ? 시간 내에 되나
-virus = []
-dfs(level, idx, lst)
-    level == M:
-        bfs(lst)
-
-    for i in range(idx, len(virus)
-
+출력
+M개의 병원을 적절히 골라 모든 바이러스를 없애는 데 필요한 최소 시간을 출력
+모든 바이러스를 없앨 수 있는 방법이 없다면 −1을 출력
 """
+
 from collections import deque
+
+def bfs(lst):
+    q = lst[:]
+    visited = [[0]*N for _ in range(N)]
+    for i, j in q:
+        visited[i][j] = 1
+    cnt = 0
+    time = 0
+    while q:
+        nq = []
+
+        for cr, cc, in q:
+            for di, dj in (-1, 0), (0, 1), (1, 0), (0, -1):
+                du, dv = cr+di, cc+dj
+                if du<0 or dv<0 or du>=N or dv>=N:
+                    continue
+                if visited[du][dv] or arr[du][dv] ==1:
+                    continue
+                visited[du][dv] = 1
+                nq.append((du, dv))
+                if arr[du][dv] == 0:
+                    cnt += 1
+        time+=1
+        if cnt==virus_cnt:
+            break
+        q = nq
+
+    return cnt==virus_cnt, time
+
+
 def dfs(level, idx, lst):
     global answer
-    if level == M:
-        s = bfs(lst)
-        answer = min(s, answer)
+    if level==M:
+        flag, time = bfs(lst)
+        if flag:
+            answer = min(answer, time)
         return
-    for i in range(idx, v):
-        dfs(level+1, i+1, lst+[i])
 
-#lst에 고른 바이러스들 활성화시켜서 퍼뜨리고 전체 소요된 시간 반환
-def bfs(lst):
-    q = deque([])
-    visited = [[0]*N for _ in range(N)]
-    b = blank
-    for i in lst:
-        r, c = virus[i]
-        q.append([r, c, 0])
-        visited[r][c] = 1
+    for i in range(idx, len(hospital_lst)):
+        dfs(level+1, i+1, lst+[hospital_lst[i]])
 
-    while q:
-        r, c, rank = q.popleft()
-        for di, dj in (-1, 0), (0, 1), (1, 0), (0, -1):
-            du = r+di
-            dv = c+dj
-            if du<0 or dv<0 or du>=N or dv>=N:
-                continue
-            if visited[du][dv] or arr[du][dv]==1:
-                continue
-            if arr[du][dv] == 0:
-                b-=1
-                if b==0:
-                    return rank+1
-            visited[du][dv] = 1
-            q.append((du, dv, rank+1))
-    return N*N+1
-N, M = map(int,input().split())
+N, M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
-virus = []
-blank = 0
-answer = N*N+1
+hospital_lst = []
+virus_cnt = 0
 for i in range(N):
     for j in range(N):
-        if arr[i][j] == 0:
-            blank+=1
-        elif arr[i][j]==2:
-            virus.append((i, j))
-v = len(virus)
-if blank == 0:
-    print(0)
-else:
+        if arr[i][j] == 2:
+            hospital_lst.append((i, j))
+        elif arr[i][j] == 0:
+            virus_cnt+=1
+answer=  N*N
+if virus_cnt!=0:
     dfs(0, 0, [])
-    print(answer if answer!=N*N+1 else -1)
+else:
+    answer = 0
+print(answer if answer != N*N else -1)
