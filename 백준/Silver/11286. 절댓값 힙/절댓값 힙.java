0e7244c7563198b3aa52afc14a467c45ffa085ba
yy <109445759+SquareOfy@users.noreply.github.com>
2025-04-15 17:33:17 +0900

    [Silver I] Title: 절댓값 힙, Time: 840 ms, Memory: 104764 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,38 +1,40 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
 import java.util.Comparator;
 import java.util.PriorityQueue;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		int N = sc.nextInt();
+		
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>(
+				new Comparator<Integer>() {
+					public int compare(Integer o1, Integer o2) {
+						int abs1 = Math.abs(o1);
+						int abs2 = Math.abs(o2);
+						if (abs1==abs2) {
+							return o1-o2;
+						}
+						return abs1-abs2;
+					}
+		});
+		
+		for(int i=0; i<N; i++) {
+			int x = sc.nextInt();
+			if (x!=0) {
+				pq.add(x);
+			}else {
+				if (pq.isEmpty()) {
+					sb.append(0);
+				}else {
+					sb.append(pq.poll());
+				}
+				sb.append("\n");
+			}
+		}
+		System.out.println(sb.toString());
+	}
 
-        StringBuilder sb = new StringBuilder();
-        int N = Integer.parseInt(br.readLine());
-
-        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                if(Math.abs(o1)-Math.abs(o2)!=0) return Math.abs(o1)-Math.abs(o2);
-                else return o1-o2;
-            }
-        });
-
-        for(int n=0; n<N; n++){
-            int num = Integer.parseInt(br.readLine());
-
-            if(num == 0 ){
-
-                if(!pq.isEmpty()) sb.append(pq.poll()).append("\n");
-                else sb.append(0).append("\n");
-            }
-            else {
-                pq.add(num);
-            }
-
-        }
-
-        System.out.println(sb.toString());
-    }
 }
