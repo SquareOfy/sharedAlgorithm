6602049c5c9ab0935eb201b53b15ab5a33a51dbe
yy <109445759+SquareOfy@users.noreply.github.com>
2025-04-15 16:40:58 +0900

    [Silver I] Title: 미로 탐색, Time: 200 ms, Memory: 18400 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,68 +1,78 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
-public class Main{
-	static int N;
-	static int M;
+public class Main {
+	
+	static int N, M;
+	static int[] dr = {-1, 0, 1, 0};
+	static int[] dc = {0, -1, 0, 1};
 	static int[][] map;
-	static boolean[][] visited;
-	static Queue<Integer[]> queue = new LinkedList<Integer[]>();
-	static int[] dr = {-1, 1, 0, 0};
-	static int[] dc = {0, 0, -1, 1};
-	static int cnt;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+	
+	static class Node{
+		int r;
+		int c;
+		int cost;
+		Node(int r, int c, int cost){
+			this.r = r;
+			this.c = c;
+			this.cost = cost;
+		}
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
 		
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+		N = sc.nextInt();
+		M = sc.nextInt();
+		sc.nextLine();
 		
-		map = new int[N+2][M+2];
-		visited = new boolean[N+2][M+2];
-		cnt = 0;
-		for(int r=1; r<=N; r++ ) {
-			String str = br.readLine();
-			for(int c=1; c<=M; c++) {
-				map[r][c]=str.charAt(c-1)-'0';
+		map = new int[N][M];
+		for(int i=0; i<N; i++) {
+			String str = sc.nextLine();
+			for(int j=0; j<M; j++) {
+				map[i][j] = str.charAt(j)-'0';
 			}
 		}
 		
-		BFS(1,1);
-	
+		int answer = bfs(0, 0);
+		System.out.println(answer);
+		
+		
 		
 	}
 	
-	static void BFS(int r, int c) {
-		queue.add(new Integer[] {r, c, 1});
+	public static int bfs(int r, int c) {
+		Queue<Node> q = new LinkedList<Node>();
+		q.add(new Node(r, c, 1));
+		boolean[][] visited = new boolean[N][M];
 		visited[r][c] = true;
-	
 		
-		while(!queue.isEmpty()) {
-			Integer[] curr = queue.poll();
+		while(!q.isEmpty()) {
+			Node now = q.poll();
+			
+			if(now.r == N-1 && now.c == M-1) {
+				return now.cost;
+			}
 			
 			for(int i=0; i<4; i++) {
-				int du = curr[0]+dr[i];
-				int dv = curr[1]+dc[i];
-				if(map[du][dv] ==1 && !visited[du][dv]) {
-					queue.add(new Integer[] {du, dv, curr[2]+1});
-					visited[du][dv] = true;
+				int du = now.r+dr[i];
+				int dv = now.c+dc[i];
+				
+				if(du<0 || du>=N || dv<0 || dv>=M) continue;
+				if(visited[du][dv]) continue;
+				if(map[du][dv]==0) continue;
 
-					if(du==N && dv==M) {
-						System.out.println(curr[2]+1);
-						break;
-					}
-				}
+				visited[du][dv] = true;
+				q.add(new Node(du, dv, now.cost+1));
+				
 			}
 			
 		}
 		
+		return -1;
 	}
 
 }
