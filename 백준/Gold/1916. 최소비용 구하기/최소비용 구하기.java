f847dbe41f29b5af5f2ccac09960c9dcd7a79c5a
yy <109445759+SquareOfy@users.noreply.github.com>
2025-04-16 21:15:07 +0900

    [Gold V] Title: 최소비용 구하기, Time: 480 ms, Memory: 50944 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,74 +1,68 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class Main {
-
+public class Main{
+	static class Node{
+		int city;
+		long cost;
+		Node(int city, long cost){
+			this.city = city;
+			this.cost = cost;
+		}
+	}
+	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
-		int N = Integer.parseInt(br.readLine()); //도시 수(노드 수) 
-		int M = Integer.parseInt(br.readLine()); //버스 수(간선 수) 
-		int[][] adj = new int[N+1][N+1];
-		for(int i=1; i<N+1; i++) {
-			
-			Arrays.fill(adj[i], -1);
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+		
+		ArrayList<Node>[] edges = new ArrayList[N+1];
+		for(int i=1; i<=N; i++) {
+			edges[i] = new ArrayList<Node>();
 		}
-		for(int m=0; m<M; m++) {
-			st=new StringTokenizer(br.readLine());
+		
+		for(int i=0; i<M; i++) {
+			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-		
-			if(adj[a][b]!= -1 && adj[a][b]<c) continue;
-			adj[a][b]=c;
-		
+			long cost = Long.parseLong(st.nextToken());
+			edges[a].add(new Node(b, cost));
 		}
-		
-		final int INF = Integer.MAX_VALUE;
-	
-		
 		
 		st = new StringTokenizer(br.readLine());
 		int start = Integer.parseInt(st.nextToken());
 		int end = Integer.parseInt(st.nextToken());
 		
-		int[] dist = new int[N+1];
-		boolean[] visited = new boolean[N+1];
-		Arrays.fill(dist, INF);
+		
+		PriorityQueue<Node> pq = new PriorityQueue<Node>((o1, o2) -> Long.compare(o1.cost, o2.cost));
+		long[] dist = new long[N+1];
+		Arrays.fill(dist, Long.MAX_VALUE);
 		dist[start] = 0;
+		pq.add(new Node(start, 0));
 		
-		
-		for(int n=1; n<=N; n++) {
-			int current = -1;
-			int min = INF;
+		while(!pq.isEmpty()) {
+			Node cur = pq.poll();
 			
-			for(int i=1; i<=N; i++) {
-				if(!visited[i] && min>dist[i]) {
-					min = dist[i];
-					current = i;
+			if(dist[cur.city] < cur.cost) continue;
+			
+			for(Node next : edges[cur.city]) {
+				if (next.cost+cur.cost < dist[next.city]) {
+					dist[next.city] = next.cost+cur.cost;
+					pq.add(new Node(next.city, dist[next.city]));
 				}
 			}
-			
-			if(current == -1) {
-	
-				break;
-			}
-			visited[current] = true;
-			
-			for(int i=1; i<=N; i++) {
-				if(!visited[i] && adj[current][i] != -1 && dist[i] > min+adj[current][i]) {
-					dist[i] = min + adj[current][i];
-					}
-			}
-			
 		}
-	
 		
 		System.out.println(dist[end]);
+		
+		
+		
 	}
-
 }
