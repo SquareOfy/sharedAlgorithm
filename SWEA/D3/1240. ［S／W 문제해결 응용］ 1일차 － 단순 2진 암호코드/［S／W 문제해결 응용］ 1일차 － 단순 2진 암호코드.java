9e36cfe6a8167d7a137d20265ab175504d246a46
yy <109445759+SquareOfy@users.noreply.github.com>
2025-04-23 16:00:09 +0900

    [D3] Title: [S/W 문제해결 응용] 1일차 - 단순 2진 암호코드, Time: 115 ms, Memory: 28,772 KB -BaekjoonHub

--- SWEA/D3/1240. ［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드/［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드.java
+++ SWEA/D3/1240. ［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드/［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드.java
@@ -1,92 +1,62 @@
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Solution{
-	static int[][] decodeArr = new int[][] {{3, 2, 1, 1}, {2, 2, 2, 1}, {2, 1, 2, 2},
-			{1, 4, 1, 1}, {1, 1, 3, 2}, {1, 2, 3, 1}, {1, 1, 1, 4}, 
-			{1, 3, 1, 2}, {1, 2, 1, 3}, {3, 1, 1, 2}
-	};
+	
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		StringBuilder sb = new StringBuilder();
 		
+		String[] nums = {"0001101", "0011001", "0010011","0111101","0100011","0110001","0101111","0111011","0110111","0001011"};
+		HashMap<String, Integer> decodeMap = new HashMap<String, Integer>();
+		for(int i=0; i<10; i++) {
+			decodeMap.put(nums[i], i);
+		}
+		
 		int T = sc.nextInt();
-		int N = 0;
-		int M = 0;
 		for(int tc =1; tc<=T; tc++) {
-			N = sc.nextInt();
-			M = sc.nextInt();
+			int N = sc.nextInt();
+			int M = sc.nextInt();
 			
-			int[][] code = new int[N][M];
+			String[] codes = new String[N];
 			for(int i=0; i<N; i++) {
-				String input = sc.next();
-				for(int j =0; j<M; j++) {
-					code[i][j] = input.charAt(j)-'0';
-				}
+				codes[i] = sc.next();
 			}
 			
-			int zeroCnt = 0;
-			int oneCnt = 0;
 			
-			int[] cntArr = new int[4];
-			int cntIdx = 3;
-			boolean startFlag = false;
-			boolean zeroFlag = false;
-			int decodeIdx = 7;
 			int[] decode = new int[8];
-			
-			for(int i=0; i<N; i++) {
-				for(int j=M-1; j>=7; j--) {
-					
-					//시작하자
-					if(code[i][j]==1) {
-						startFlag=true;
-						for(int k=0; k<7; k++) {
-							if(code[i][j-k]==0) {
-								zeroCnt++;
-								if(!zeroFlag) {
-									cntArr[cntIdx--] = oneCnt;
-									zeroFlag = true;
-								}
-								oneCnt=0;
-							}else {
-								oneCnt++;
-								startFlag = true;
-								if(zeroFlag) {		
-									cntArr[cntIdx--] = zeroCnt;
-									zeroFlag = false;
-								}
-								zeroCnt=0;
-							}
-						}
-						cntArr[0] = 7-(cntArr[1]+cntArr[2]+cntArr[3]);
-						cntIdx = 3;
-						zeroCnt = 0;
-						zeroFlag =false;
-						oneCnt = 0;
-						decode[decodeIdx--]= decode(cntArr);
-						j -=6;
-					}
-					
-				}
-				if(startFlag) break;
-			}
-			
-
+			int decodeIdx = 7;
 			int odd = 0;
 			int even = 0;
-			//옳은 암호인지 판독하기
-			for(int i=0; i<8; i++) {
-				if(i%2==0) {
-					odd += decode[i];
-				}else {
-					even +=decode[i];
+			
+			for(int i=0; i<N; i++) {
+				if(!codes[i].contains("1")) continue;
+				int lastIdx = -1;
+				for(int j=M-1; j>=0; j--) {
+					if(codes[i].charAt(j)=='1') {
+						lastIdx = j+1;
+						break;
+					}
 				}
+				
+				while(decodeIdx!=-1) {
+					decode[decodeIdx] = decodeMap.get(codes[i].substring(lastIdx-7, lastIdx));
+					if(decodeIdx%2==0) {
+						odd+=decode[decodeIdx];
+					}else {
+						even+=decode[decodeIdx];
+					}
+					decodeIdx--;
+					lastIdx-=7;
+				}
+				break;
 			}
-			int answer = odd*3 + even;
+			
 			sb.append("#").append(tc).append(" ");
-			if(answer%10==0) {
+			if((odd*3+even)%10==0) {
 				sb.append(odd+even);
 			}else {
 				sb.append(0);
@@ -96,14 +66,5 @@ public class Solution{
 		System.out.println(sb.toString());
 		
 	}
-	
-	static int decode(int[] cntArr) {
-		for(int i=0; i<10; i++) {
-			for(int j=0; j<4; j++) {
-				if(cntArr[j]!=decodeArr[i][j]) break;
-				if(j==3) return i;
-			}
-		}
-		return -1;
-	}
+
 }
