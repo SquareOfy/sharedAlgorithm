8f02c3db31f32dff95fbc6fd5fc1fa0e66cf89ab
yy <109445759+SquareOfy@users.noreply.github.com>
2025-04-25 16:00:56 +0900

    [D5] Title: [S/W 문제해결 응용] 2일차 - 균형점, Time: 113 ms, Memory: 29,568 KB -BaekjoonHub

---
+++ SWEA/D5/1245. ［S／W 문제해결 응용］ 2일차 － 균형점/README.md
@@ -0,0 +1,15 @@
+# [D5] [S/W 문제해결 응용] 2일차 - 균형점 - 1245 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV15MeBKAOgCFAYD) 
+
+### 성능 요약
+
+메모리: 29,568 KB, 시간: 113 ms, 코드길이: 1,288 Bytes
+
+### 제출 일자
+
+2025-04-25 16:00
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D5/1245. ［S／W 문제해결 응용］ 2일차 － 균형점/［S／W 문제해결 응용］ 2일차 － 균형점.java
@@ -0,0 +1,72 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+
+class Solution {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		
+		int T = sc.nextInt();
+		
+		for(int tc = 1; tc<=T; tc++) {
+			int N = sc.nextInt();
+			
+			double[] point = new double[N];
+			double[] weight = new double[N];
+			
+			for(int i=0; i<N; i++) {
+				point[i] = sc.nextDouble();
+			}
+			
+			for(int i=0; i<N; i++) {
+				weight[i] = sc.nextDouble();
+			}
+			
+			sb.append("#").append(tc).append(" ");
+			for(int i=0; i<N-1; i++) {
+				double left = point[i];
+				double right = point[i+1];
+				
+				double mid = 0;
+				double value = 0;
+				
+				while(left<=right) {
+					
+					mid = (left+right)/2;
+					value = 0;
+					
+					for(int j=0; j<N; j++) {
+						if(point[j] < mid) {
+							value+= weight[j]/((mid-point[j])*(mid-point[j]));
+						}else {
+							value -= weight[j]/((point[j]-mid)*(point[j]-mid));
+						}
+					}
+					
+					
+					
+					if(Math.abs(value) <= 1e-13) {
+						break;
+					}else if(value>0) {
+						left = mid+1e-13;
+					}else {
+						right = mid-1e-13;
+					}
+					
+				}
+				
+				sb.append(String.format("%.10f", mid)).append(" ");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb.toString());
+		
+			
+		
+	}
+	
+
+	
+}
